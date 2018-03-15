af7329d2606d6b4cc27fb2f8fda03886d634b9b0
Jo Meenen <jo@meenen.de>
2018-03-15 08:50:58 +0100

    Add MobileContainer component

---
+++ src/components/MobileContainer.js
@@ -0,0 +1,68 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+import {
+  Button,
+  Container,
+  Responsive,
+  Sidebar,
+  Segment,
+  Menu,
+  Icon
+} from 'semantic-ui-react'
+import HomePageHeading from '../components/HomePageHeading'
+
+class MobileContainer extends Component {
+  state = {}
+
+  handlePusherClick = () => {
+    const { sidebarOpened } = this.state
+
+    if (sidebarOpened) this.setState({ sidebarOpened: false })
+  }
+
+  handleToggle = () => this.setState({ sidebarOpened: !this.state.sidebarOpened })
+
+  render () {
+    const { children } = this.props
+    const { sidebarOpened } = this.state
+
+    return (
+      <Responsive {...Responsive.onlyMobile}>
+        <Sidebar.Pushable>
+          <Sidebar as={Menu} animation='uncover' inverted vertical visible={sidebarOpened}>
+            <Link className='item' to='/'>Home</Link>
+            <Link className='item' to='/commands'>Commands</Link>
+            <Link className='item' to='/components'>Components</Link>
+            <Link className='item' to='/github'>GitHub</Link>
+          </Sidebar>
+
+          <Sidebar.Pusher dimmed={sidebarOpened} onClick={this.handlePusherClick} style={{ minHeight: '100vh' }}>
+            <Segment inverted textAlign='center' style={{ minHeight: 350, padding: '1em 0em' }} vertical>
+              <Container>
+                <Menu inverted pointing secondary size='large'>
+                  <Menu.Item onClick={this.handleToggle}>
+                    <Icon name='sidebar' />
+                  </Menu.Item>
+                  <Menu.Item position='right'>
+                    <Button as='a' inverted>Log in</Button>
+                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>Sign Up</Button>
+                  </Menu.Item>
+                </Menu>
+              </Container>
+              <HomePageHeading mobile />
+            </Segment>
+
+            {children}
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Responsive>
+    )
+  }
+}
+
+MobileContainer.propTypes = {
+  children: PropTypes.node
+}
+
+export default MobileContainer
