4271561db3c7946dc00b568b54d3e4127d439afc
Jo Meenen <jo@meenen.de>
2018-03-15 08:43:14 +0100

    Add HomePageHeading component

---
+++ src/components/HomePageHeading.js
@@ -0,0 +1,44 @@
+import PropTypes from 'prop-types'
+import React from 'react'
+import {
+  Button,
+  Container,
+  Header,
+  Icon
+} from 'semantic-ui-react'
+
+const HomePageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='SUI Parcel'
+      inverted
+      style={{
+        fontSize: mobile ? '2em' : '4em',
+        fontWeight: 'normal',
+        marginBottom: 0,
+        marginTop: mobile ? '1.5em' : '3em'
+      }}
+    />
+    <Header
+      as='h2'
+      content='Launch your own React projects in no time with SUI Parcel.'
+      inverted
+      style={{
+        fontSize: mobile ? '1.5em' : '1.7em',
+        fontWeight: 'normal',
+        marginTop: mobile ? '0.5em' : '1.5em'
+      }}
+    />
+    <Button primary size='huge'>
+      Get Started
+      <Icon name='right arrow' />
+    </Button>
+  </Container>
+)
+
+HomePageHeading.propTypes = {
+  mobile: PropTypes.bool
+}
+
+export default HomePageHeading
