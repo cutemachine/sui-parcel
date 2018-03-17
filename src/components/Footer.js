c3fe4dec106b24938a61518eac2548f91df2e124
Jo Meenen <jo@meenen.de>
2018-03-17 15:27:33 +0100

    Change background color of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ import {
 } from 'semantic-ui-react'
 
 const Footer = () => (
-  <Segment inverted vertical style={{ padding: '5em 0em' }}>
+  <Segment inverted vertical style={{ padding: '5em 0em', backgroundColor: '#263749' }}>
     <Container>
       <Grid divided inverted stackable>
         <Grid.Row>
