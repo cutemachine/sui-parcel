11d3fac11d5e6e5ae062c3f896aaa06e8976d005
Jo Meenen <jo@meenen.de>
2018-03-17 17:54:23 +0100

    Add back button to second page

--- src/pages/ComponentsPage.js
+++ src/pages/ComponentsPage.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router'
+import { Button } from 'semantic-ui-react'
 
 export default class ComponentsPage extends Component {
   render () {
     return (
-      <h1>ComponentsPage</h1>
+      <div>
+        <h1>ComponentsPage</h1>
+        <p>This is a second page and demonstrates the React Router.</p>
+        <Button as={Link} to='/' style={{marginLeft: '0.5em'}}>
+          Back to home
+        </Button>
+      </div>
     )
   }
 }
